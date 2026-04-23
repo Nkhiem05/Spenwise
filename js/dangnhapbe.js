f2cb1f8f6c2852922e6e91b8316419b730f0f513
Nkhiem05 <phankhuem@gmail.com>
2026-04-23 22:43:38 +0700

    sua loi session getitem

--- js/dangnhapbe.js
+++ js/dangnhapbe.js
@@ -1,6 +1,6 @@
 function dangnhap() {
   const clientid =
     "427841697794-g1t56d2mvk47iqgntj2d6t6sdhc9oj0u.apps.googleusercontent.com";
-  let link = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&response_type=token&redirect_uri=http://127.0.0.1:3000/page/dkdngg.html&client_id=${clientid}`;
+  let link = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&response_type=token&redirect_uri=https://nkhiem05.github.io/Spenwise/page/dkdngg.html&client_id=${clientid}`;
   window.location.href = link;
 }
