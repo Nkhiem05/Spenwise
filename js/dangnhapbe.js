beb454633bcb6544e811ebeeccb0f584be532f8f
Nkhiem05 <phankhuem@gmail.com>
2026-04-23 19:26:08 +0700

    viet be dangnhap

---
+++ js/dangnhapbe.js
@@ -0,0 +1,10 @@
+function dangnhap() {
+  const clientid =
+    "427841697794-g1t56d2mvk47iqgntj2d6t6sdhc9oj0u.apps.googleusercontent.com";
+  let link = `https://accounts.google.com/o/oauth2/v2/auth?
+ scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile&
+ response_type=token&
+ redirect_uri=https://nkhiem05.github.io/Spenwise/page/dkdngg.html&
+ client_id=${clientid}`;
+  window.location.href = link;
+}
